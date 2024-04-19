0f2776f09b0e69fde66d0653689b69ac7c4bc5a2
Haruchon <brunonsho@gmail.com>
2024-04-18 22:00:37 -0500

    added header and a bit of styling

---
+++ src/components/header/header.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import "./header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="navbar-wrapper">
+          <nav className="navbar">
+            <ul className="nav-menu">
+              <li className="nav-list">
+                <Link href="/">Home</Link>
+              </li>
+              <li className="nav-list">
+                <Link href="/projects">Projects</Link>
+              </li>
+              <li className="nav-list">
+                <Link href="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
