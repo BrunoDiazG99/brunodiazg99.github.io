d2fd00b74b1e52913601089c01eee869ac31c76a
Haruchon <brunonsho@gmail.com>
2024-07-22 12:47:21 -0500

    just an image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import "./home.css";
+import Image from "next/image";
+import justPic from "../../public/haruchon.png";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
-      <p>test home</p>
+      <div className="home-container">
+        <div className="home-desc">
+          <p>
+            Hi!
+            <br />
+            I&apos;m Bruno a Web Developer from Peru! ... Lorem ipsum /
+          </p>
+        </div>
+        <div className="home-pic">
+          <Image src={justPic} alt="just a pic" width={236} height={296} />
+        </div>
+      </div>
     </main>
   );
 }
